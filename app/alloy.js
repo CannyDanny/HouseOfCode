408feac83653fdceebfd18e3035e5977c2779b2c
David Wolff <info@davidwolff.dk>
2017-04-03 11:13:17 +0200

    addaded global colors

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,19 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+// Global colors
+Alloy.Globals.viewTitle = "#54d4d4"; // lightblue
+Alloy.Globals.blueText = "#66dad4"; // lightblue text, different from viewTitle
+Alloy.Globals.whiteText = "#ffffff"; // plain white text
+Alloy.Globals.blueElement = "#58d3ca"; // lightblue color for elements, like button and back arrow
+Alloy.Globals.redElement = "#b83030"; // red color, for the delete button
+Alloy.Globals.tabIcon = "#172935"; // darkblue color for the icon in the tabGroup at the bottom
+Alloy.Globals.tabIconBackground = "#192834"; // darkblue color for the tappend icons in the bottom
+Alloy.Globals.backgroundColor = "#182b38"; // background color for all the views
+
+// USAGE
+// In a *.tss file you can write the following to get a specific color:
+// ".Label": {
+// 	color: Alloy.Globals.blueElement
+// }
