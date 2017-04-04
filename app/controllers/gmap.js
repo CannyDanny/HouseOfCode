c460971b9be98c15bbdde5cbe7125f87f21e47eb
David Wolff <info@davidwolff.dk>
2017-04-04 11:13:25 +0200

    still working

--- app/controllers/gmap.js
+++ app/controllers/gmap.js
@@ -2,7 +2,26 @@ var googleAPIKey = "AIzaSyBotnDBlQ21qy9VAwIr81RpCW21HMSIxHk";
 
 var MapModule = require('ti.map');
 
-
+var rc = MapModule.isGooglePlayServicesAvailable();
+switch (rc) {
+    case MapModule.SUCCESS:
+        Ti.API.debug('Google Play services is installed.');
+        break;
+    case MapModule.SERVICE_MISSING:
+        alert('Google Play services is missing. Please install Google Play services from the Google Play store.');
+        break;
+    case MapModule.SERVICE_VERSION_UPDATE_REQUIRED:
+        alert('Google Play services is out of date. Please update Google Play services.');
+        break;
+    case MapModule.SERVICE_DISABLED:
+        alert('Google Play services is disabled. Please enable Google Play services.');
+        break;
+    case MapModule.SERVICE_INVALID:
+        alert('Google Play services cannot be authenticated. Reinstall Google Play services.');
+        break;
+    default:
+        alert('Unknown error.');
+}
 
 // var win = Ti.UI.createWindow();
 var opera = MapModule.createAnnotation({
@@ -47,11 +66,13 @@ var random = MapModule.createAnnotation({
 });
 
 var mapview = MapModule.createView({
+    mapType: MapModule.NORMAL_TYPE,
     region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-    annotations: [bridge,opera] //< add these annotations upon creation
+    annotations: [bridge,opera], //< add these annotations upon creation
+
 });
 
 // // Add this annotation after creation
+Ti.API.debug("test");
 mapview.addAnnotation(random);
 $.gmapView.add(mapview);
-// win.open();
