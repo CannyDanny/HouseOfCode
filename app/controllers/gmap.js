04b9428efd3e49e37b087bb620dc21198017629d
David Wolff <info@davidwolff.dk>
2017-04-04 14:16:28 +0200

    Getting the address from gps in readable form - requires internet access

--- app/controllers/gmap.js
+++ app/controllers/gmap.js
@@ -2,34 +2,78 @@ var googleAPIKey = "AIzaSyBotnDBlQ21qy9VAwIr81RpCW21HMSIxHk";
 
 var MapModule = require('ti.map');
 
+// http://www.titaniumtutorial.com/2012/04/google-geocode-in-titanium-application.html
 
-(function() {
+// http://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=55.401756,10.433579
+
 
     $.mapview.addEventListener('click', function (event) {
-    	Ti.API.error('Calling Ti.Geolocation.getCurrentPosition');
+    	// Ti.API.error('Calling Ti.Geolocation.getCurrentPosition');
     	if (Ti.Geolocation.locationServicesEnabled) {
     		Ti.Geolocation.getCurrentPosition(function (event) {
-    			Ti.API.error('Ti.Geolocation.getCurrentPosition Callback Success: ' + event.success);
 
-    	    	Ti.UI.createAlertDialog({
-    	      		title: 'Geolocation',
-    	      		message: JSON.stringify(event.coords),
-    	      		ok: 'OK'
-    	    	}).show();
+                var addrUrl = "http://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng="+event.coords.latitude+","+event.coords.longitude;
+                /* web-service call */
+                var addrReq = Titanium.Network.createHTTPClient();
+                addrReq.open("GET",addrUrl);
+                addrReq.send(null);
 
+                addrReq.onload = function()
+                {
+                    var response = JSON.parse(this.responseText);
 
-    			if (event.error) {
-    	            Ti.API.error('Error: ' + event.error);
-    	        } else {
-    	            Ti.API.info(JSON.stringify(event.coords));
-    	        }
+                    if(response.status == "OK"){
+                        var resLen = response.results[0].address_components.length;
+                        for(var i=0; i < resLen; i++) {
+                            switch (response.results[0].address_components[i].types[0])
+                            {
+                                case "street_number":
+                                    Ti.API.info("street number : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                case "route":
+                                    Ti.API.info("street name : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                case "locality":
+                                    Ti.API.info("city name : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                case "administrative_area_level_1":
+                                    Ti.API.info("state name : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                case "postal_code":
+                                    Ti.API.info("zip code : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                case "country":
+                                    Ti.API.info("country name : "+response.results[0].address_components[i].long_name);
+                                    break;
+                                }
+                        }
+                    }else{
+                        showAlert('','Unable to find Address');
+                    }
+
+                };
+
+    			// Ti.API.info('Ti.Geolocation.getCurrentPosition Callback Success: ' + event.success);
+                //
+    	    	// Ti.UI.createAlertDialog({
+                //     title: 'Geolocation',
+                //     message: JSON.stringify(event.coords),
+                //     ok: 'OK'
+    	    	// }).show();
+                //
+                //
+    			// if (event.error) {
+    	        //     Ti.API.info('Error: ' + event.error);
+    	        // } else {
+    	        //     Ti.API.info(JSON.stringify(event.coords));
+    	        // }
     	    });
     	}else{
     		Ti.Geolocation.requestLocationPermissions();
     	}
     });
 
-}());
+
 //
 // var rc = MapModule.isGooglePlayServicesAvailable();
 // switch (rc) {
